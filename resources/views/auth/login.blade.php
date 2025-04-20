82abc65393bccdae195f1dfc99e065181643d07d
LeonRado1 <radoleon2@gmail.com>
2025-04-20 14:53:20 +0200

    login and signup pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,61 @@
 <x-app-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <main class="container row py-5 my-5 justify-content-center mx-auto">
+        <form class="col-lg-4 p-4 bg-light rounded shadow-sm" method="POST" action="{{ route('login') }}">
+            @csrf
+            <h3 class="fs-3 text-secondary text-center mb-3">Prihlásenie</h3>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input
+                    name="email"
+                    type="email"
+                    class="form-control @error('email') is-invalid @enderror"
+                    id="email"
+                    placeholder="Zadajte email"
+                    value="{{ old('email') }}"
+                    required
+                    autofocus
+                >
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Heslo</label>
+                <input
+                    name="password"
+                    type="password"
+                    class="form-control @error('password') is-invalid @enderror"
+                    id="password"
+                    placeholder="Zadajte heslo"
+                    required
+                >
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mt-3 form-check">
+                <input 
+                    class="form-check-input" 
+                    type="checkbox" 
+                    name="remember" 
+                    id="remember"
+                    {{ old('remember') ? 'checked' : '' }}
+                >
+                <label class="form-check-label" for="remember">
+                    Zapamätať si ma
+                </label>
+            </div>
+            <button
+                type="submit"
+                class="btn btn-primary mt-3 col-12"
+            >
+                <i class="bi bi-box-arrow-in-right"></i>
+                Prihlásiť sa
+            </button>
+            <p class="mt-3 text-center">
+                Nemáte ešte účet?
+                <a class="link-primary" href="{{ route('register') }}">Zaregistrovať sa</a>
+            </p>
+        </form>
+    </main>
 </x-app-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,72 @@
 <x-app-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <main class="container row py-5 my-5 justify-content-center mx-auto">
+        <form method="POST" action="{{ route('register') }}" class="col-lg-4 p-4 bg-light rounded shadow-sm">
+            @csrf
+            <h3 class="fs-3 text-secondary text-center mb-3">Registrácia</h3>
+            <div class="mb-3">
+                <label for="name" class="form-label">Používateľské meno</label>
+                <input
+                    type="text"
+                    class="form-control @error('name') is-invalid @enderror"
+                    id="name"
+                    name="name"
+                    value="{{ old('name') }}"
+                    placeholder="Zadajte meno"
+                    required
+                    autofocus
+                >
+                @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input
+                    type="email"
+                    class="form-control @error('email') is-invalid @enderror"
+                    id="email"
+                    name="email"
+                    value="{{ old('email') }}"
+                    placeholder="Zadajte email"
+                    required
+                >
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Heslo</label>
+                <input
+                    type="password"
+                    class="form-control @error('password') is-invalid @enderror"
+                    id="password"
+                    name="password"
+                    placeholder="Zadajte heslo"
+                    required
+                >
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Zopakujte heslo</label>
+                <input
+                    type="password"
+                    class="form-control"
+                    id="password_confirmation"
+                    name="password_confirmation"
+                    placeholder="Zopakujte heslo"
+                    required
+                >
+            </div>
+            <button type="submit" class="btn btn-success mt-3 col-12">
+                <i class="bi bi-person-fill"></i>
+                Zaregistrovať sa
+            </button>
+            <p class="mt-3 text-center">
+                Už máte účet?
+                <a class="link-primary" href="{{ route('login') }}">Prihlásiť sa</a>
+            </p>
+        </form>
+    </main>
 </x-app-layout>
